9277140939cede05c1f04b7690488ca94a4e417e
davidlozada0610-debug <davidlozada0610@gmail.com>
2026-04-17 11:30:01 -0400

    agregando manipulacion de texto

--- talleriujo01/talleriujo01/Program.cs
+++ talleriujo01/talleriujo01/Program.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: usuario
- * Fecha: 17/4/2026
- * Hora: 10:53 a. m.
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
+﻿
 using System;
 
 namespace talleriujo01
@@ -14,9 +7,25 @@ namespace talleriujo01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("===taller01 ===");
 			
-			// TODO: Implement Functionality Here
+			
+			// 1. El dato del usuario
+			string registroUsuario = "    ID_777;  DavidLozada  ;  Evaluacion;    95";
+			
+			Console.WriteLine(registroUsuario);
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes [2].Trim();
+			string nota = partes [3].Trim();
+			
+			
+			
+			Console.WriteLine(string.Format("el id es: {0} del usuario {1} con la nota {2}", id,nombre,nota));
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
